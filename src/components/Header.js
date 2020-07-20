e375fa4da22d0b2398438dbb36edd3eb50fde31a
Ubaidhullah <ubaidhullah05@gmail.com>
2020-07-20 06:00:19 +0500

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <div>
+      <h2>Expence Traker</h2>
+    </div>
+  );
+};
---
+++ src/context/GlobalState.js
@@ -0,0 +1,38 @@
+import React, { createContext, useReducer } from "react";
+import AppReducer from "./AppReducer";
+const InitialSate = {
+  transactions: [],
+};
+
+//create context
+export const GlobalContext = createContext(InitialSate);
+
+export const GlobalProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(AppReducer, InitialSate);
+
+  function deleteTransaction(id) {
+    dispatch({
+      type: "DELETE_TRANSACTION",
+      payload: id,
+    });
+  }
+
+  function addTransaction(transaction) {
+    dispatch({
+      type: "ADD_TRANSACTION",
+      payload: transaction,
+    });
+  }
+
+  return (
+    <GlobalContext.Provider
+      value={{
+        transactions: state.transactions,
+        deleteTransaction,
+        addTransaction,
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
+};
